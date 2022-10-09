625c9ca248162744e71925f995b0d7d8b27af3c1
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 19:21:24 +0300

    Organization <> removed all unuseful comments from the routes

--- elearning_server/routes/api.php
+++ elearning_server/routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'v0.1'], function () {
 
-    // Route::group(['middleware' => 'auth:api'], function () {
-    //     Route::get('/homepage', [UserController::class, 'getHomepage'])->name('homepage');
-    // });
-    
     //Admins routes
     Route::group(['prefix' => 'admin'], function () {
         Route::group(['middleware' => 'admin.role'], function () {
@@ -31,18 +27,6 @@ Route::group(['prefix' => 'v0.1'], function () {
         Route::group(['middleware' => 'student.role'], function () {
         });
     });
-
-
-
-    //TODO : authorize only admins and instructors to add students
-    // Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
-    //TODO : only authorize admins to register admins
-    // Route::post('/register_admin', [AuthController::class, 'registerAdmin'])->name('register-admin');
-    //TODO : only authorize admins to register instructors
-    // Route::post('/register_instructor', [AuthController::class, 'registerInstructor'])->name('register-instructor');
-
-
-
 
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
