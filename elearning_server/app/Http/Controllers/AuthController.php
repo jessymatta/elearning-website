5a4257f7346d4f33dce7a536117a2a9080c2d0f0
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 18:27:25 +0300

    Backend <> Students Registration : added a method that will add register students

--- elearning_server/app/Http/Controllers/AuthController.php
+++ elearning_server/app/Http/Controllers/AuthController.php
@@ -47,11 +47,10 @@ class AuthController extends Controller
             'password'=>'required'
         ]);
 
-        //if validation fails
         if($validator->fails()){
             return response()->json($validator->errors()->toJson(), 422);
         }
-        //if user is not authorized
+
         if(!$token=auth() -> attempt($validator->validated())){
             return response()->json(['error'=>'Unauthorized'], 401);
         }
@@ -67,5 +66,38 @@ class AuthController extends Controller
             'user'=>auth()->user()
         ]);
     }
+
+    //A function that will add a new student
+    public function registerStudent(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'username' => 'required|string|unique:users',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            "email" => $request->email,
+            "username" => $request->username,
+            "user_type"=> "student",
+            "password" => bcrypt($request->password)
+
+        ]);
+
+        return response()->json([
+            'message'=>'Student successfully registered',
+            'status'=>"success",
+            'student'=>$user
+        ], 201);
+    }
+
+
+
 
 }
