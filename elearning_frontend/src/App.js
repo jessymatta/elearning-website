6d9978399c6934b921923b937ac3b1e43e225cc6
jessymatta <jessy.w.matta@gmail.com>
2022-10-10 00:28:11 +0300

    Frontend <> Students Dash : added a route to the Students dashboard

--- elearning_frontend/src/App.js
+++ elearning_frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Login from './pages/login/Login';
 import AdminDashboard from './pages/admin/AdminDashboard';
+import InstructorDashboard from './pages/instructor/InstructorDashboard';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,8 +12,10 @@ function App() {
       <Routes>
         {/* Route of the login page*/}
         <Route path="/" element={<Login />}></Route>
-        {/* Route for the homepage */}
+        {/* Route for admins dashboard */}
         <Route path="/admin/dashboard" element={<AdminDashboard />}></Route>
+        {/* Route for instructors dashboard */}
+        <Route path="/instructor/dashboard" element={<InstructorDashboard />}></Route>
       </Routes>
         {/* <Login /> */}
       </BrowserRouter>
