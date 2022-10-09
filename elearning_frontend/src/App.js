4e6607d7390a5dd177e70b7acdf5a6ab82b45aad
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 13:07:55 +0300

    Frontend <> Users Pages : added routes for the login page and the homepage

--- elearning_frontend/src/App.js
+++ elearning_frontend/src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import Login from './pages/login/Login';
+import HomePage from './pages/homepage/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <main className='App'>
-      <Login />
-    </main>
+    <div className='App'>
+      <BrowserRouter>
+      <Routes>
+        {/* Route of the login page*/}
+        <Route path="/login" element={<Login />}></Route>
+        {/* Route for the homepage */}
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+        {/* <Login /> */}
+      </BrowserRouter>
+    </div>
   );
 }
 
