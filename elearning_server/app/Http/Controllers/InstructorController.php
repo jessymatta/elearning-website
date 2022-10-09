fed6c6b77628d3db0fd0260af0324d075dabe6fd
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 19:57:58 +0300

    Backend <> Get Instructors : added a method that will retrieve all instructors

--- elearning_server/app/Http/Controllers/InstructorController.php
+++ elearning_server/app/Http/Controllers/InstructorController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class InstructorController extends Controller
 {
-    //
+    public function getAllInstructors()
+    {
+        $instructors = User::where('user_type',"instructor")->get();
+        return response()->json([
+            "status" => "success",
+            "instructors" => $instructors
+        ], 200);
+    }
+
 }
