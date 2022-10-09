d8e47cf2ed0a0fc81c18a9dbe6e46b72a31f8ab5
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 18:45:25 +0300

    Backend <> Users Registration : temporarily moved the register routes outside the auth middleware

--- elearning_server/routes/api.php
+++ elearning_server/routes/api.php
@@ -8,13 +8,11 @@ Route::group(['prefix' => 'v0.1'], function () {
 
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('/homepage', [UserController::class, 'getHomepage'])->name('homepage');
-        //TODO : authorize only admins and instructors to add students
-        Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
-        //TODO : only authorize admins to register admins
-        Route::post('/register_admin', [AuthController::class, 'registerAdmin'])->name('register');
     });
-    
 
-
+    //TODO : authorize only admins and instructors to add students
+    Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
+    //TODO : only authorize admins to register admins
+    Route::post('/register_admin', [AuthController::class, 'registerAdmin'])->name('register-admin');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
