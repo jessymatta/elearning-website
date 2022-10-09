ef1e0ffa3fc02401f8c74f93d46896d840caeec7
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 18:39:17 +0300

    Backend <> Users DB : removed the required user_type field from the users table

--- elearning_server/database/migrations/2014_10_12_000000_create_users_table.php
+++ elearning_server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('user_type');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
