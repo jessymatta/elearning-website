576e95f32925e2146fdc7eddce3923931b56da42
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 18:33:31 +0300

    Backend <> Students Registration : added a route to register students

--- elearning_server/routes/api.php
+++ elearning_server/routes/api.php
@@ -5,10 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'v0.1'], function () {
-    
+
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('/homepage', [UserController::class, 'getHomepage'])->name('homepage');
+        //TODO : authorize only admins and instructors to add students
+        Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
     });
+    
 
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
