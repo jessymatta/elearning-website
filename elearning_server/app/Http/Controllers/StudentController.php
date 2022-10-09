a0e56ec29c2006f36a892b4e4ebe9316f0ed8c93
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 19:50:40 +0300

    Backend <> Students : added a method that will retrieve all students

--- elearning_server/app/Http/Controllers/StudentController.php
+++ elearning_server/app/Http/Controllers/StudentController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class StudentController extends Controller
 {
-    //
+    public function getAllStudents()
+    {
+        $students = User::where('user_type',"student")->get();
+        return response()->json([
+            "status" => "success",
+            "students" => $students
+        ], 200);
+    }
+
 }
