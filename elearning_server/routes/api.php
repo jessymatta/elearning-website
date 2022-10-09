57237c7b56f2d543318a09edb64fb140b6f1cfff
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 19:19:58 +0300

    Backend <> Users Auth : arranged the routes according to the customs middlewares

--- elearning_server/routes/api.php
+++ elearning_server/routes/api.php
@@ -6,15 +6,43 @@ use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'v0.1'], function () {
 
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::get('/homepage', [UserController::class, 'getHomepage'])->name('homepage');
+    // Route::group(['middleware' => 'auth:api'], function () {
+    //     Route::get('/homepage', [UserController::class, 'getHomepage'])->name('homepage');
+    // });
+    
+    //Admins routes
+    Route::group(['prefix' => 'admin'], function () {
+        Route::group(['middleware' => 'admin.role'], function () {
+            Route::post('/register_admin', [AuthController::class, 'registerAdmin'])->name('register-admin');
+            Route::post('/register_instructor', [AuthController::class, 'registerInstructor'])->name('register-instructor');
+            Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
+        });
     });
 
+    //Instructors routes
+    Route::group(['prefix' => 'instructor'], function () {
+        Route::group(['middleware' => 'instructor.role'], function () {
+            Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
+        });
+    });
+
+    //Students routes
+    Route::group(['prefix' => 'student'], function () {
+        Route::group(['middleware' => 'student.role'], function () {
+        });
+    });
+
+
+
     //TODO : authorize only admins and instructors to add students
-    Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
+    // Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
     //TODO : only authorize admins to register admins
-    Route::post('/register_admin', [AuthController::class, 'registerAdmin'])->name('register-admin');
+    // Route::post('/register_admin', [AuthController::class, 'registerAdmin'])->name('register-admin');
     //TODO : only authorize admins to register instructors
-    Route::post('/register_instructor', [AuthController::class, 'registerInstructor'])->name('register-instructor');
+    // Route::post('/register_instructor', [AuthController::class, 'registerInstructor'])->name('register-instructor');
+
+
+
+
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
