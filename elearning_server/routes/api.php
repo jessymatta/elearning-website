699c0bb46aec33dc6abcaa053516b88ebd201c3e
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 18:49:35 +0300

    Backend <> Instructors Reg. : added a route to add instructors

--- elearning_server/routes/api.php
+++ elearning_server/routes/api.php
@@ -14,5 +14,7 @@ Route::group(['prefix' => 'v0.1'], function () {
     Route::post('/register_student', [AuthController::class, 'registerStudent'])->name('register-student');
     //TODO : only authorize admins to register admins
     Route::post('/register_admin', [AuthController::class, 'registerAdmin'])->name('register-admin');
+    //TODO : only authorize admins to register instructors
+    Route::post('/register_instructor', [AuthController::class, 'registerInstructor'])->name('register-instructor');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
