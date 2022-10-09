b66f74ed693554d71d8b846db7912f2439fe7600
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 22:17:00 +0300

    Backend <> Update Student : added a method that updates student info

--- elearning_server/app/Http/Controllers/StudentController.php
+++ elearning_server/app/Http/Controllers/StudentController.php
@@ -9,11 +9,24 @@ class StudentController extends Controller
 {
     public function getAllStudents()
     {
-        $students = User::where('user_type',"student")->get();
+        $students = User::where('user_type', "student")->get();
         return response()->json([
             "status" => "success",
             "students" => $students
         ], 200);
     }
 
+    public function updateStudent(Request $request, $student_id)
+    {
+        $updated_student = User::find($student_id);
+        $updated_student->name = $request->name;
+        $updated_student->email = $request->email;
+        $updated_student->save();
+
+        return response()->json([
+            "status" => "success",
+            "updated_user" => $updated_student
+        ], 200);
+
+    }
 }
