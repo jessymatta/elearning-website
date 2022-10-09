bc86cbb834ddc3094108a59c868b4c998d033fbb
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 16:56:47 +0300

    Backend  <> Users SignUp : added the register method in the AuthController

--- elearning_server/app/Http/Controllers/AuthController.php
+++ elearning_server/app/Http/Controllers/AuthController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'username' => 'required|string|unique:users',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            "email" => $request->email,
+            "username" => $request->username,
+            "password" => bcrypt($request->password)
+
+        ]);
+
+        return response()->json([
+            'message'=>'User successfully registered',
+            'status'=>"success",
+            'user'=>$user
+        ], 201);
+    }
 }
