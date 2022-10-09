842b05a23ba1d7a0a82f097878fd6cfd8b21baa3
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 19:11:40 +0300

    Backend <> Students Auth : added a custom middleware for students

--- elearning_server/app/Http/Kernel.php
+++ elearning_server/app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'admin.role' => \App\Http\Middleware\AdminMiddleware::class,
         'instructor.role' => \App\Http\Middleware\InstructorMiddleware::class,
+        'student.role' => \App\Http\Middleware\StudentMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
