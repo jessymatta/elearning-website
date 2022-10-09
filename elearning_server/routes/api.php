0a05bb2bebef0efa86ec43649dd790e34ee4b170
jessymatta <jessy.w.matta@gmail.com>
2022-10-09 16:25:22 +0300

    Backend <> Users Singup : added a route from users registration

--- elearning_server/routes/api.php
+++ elearning_server/routes/api.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::group(['prefix' => 'v0.1'], function () {
+    
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('/homepage', [UserController::class, 'getHomepage'])->name('homepage');
     });
 
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
