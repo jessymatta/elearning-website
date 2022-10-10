380b2bc1ac68e27c165d51c9cb1c40eb0ebea118
jessymatta <jessy.w.matta@gmail.com>
2022-10-10 23:29:01 +0300

    Frontend <> Admin Courses: added the courses component for the admin dashboard

---
+++ elearning_frontend/src/pages/admin/AdminDashboardCourses.js
@@ -0,0 +1,25 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Title from "../../components/header/Title";
+import './admindash.css';
+import Footer from "../../components/footer/Footer";
+import TableStudents from "../../components/table/TableStudents";
+
+const AdminDashboardCourses = () => {
+    return (
+        <div class="admin-dash">
+            <Sidebar/>
+            <div className="dash-container">
+                <Title title={"Students List"}/>
+                
+                <div className="table-container">
+                    <div className="table-title">All Courses</div>
+                    {/* <TableStudents/> */}
+                    YUP
+                </div>
+                <Footer/>
+            </div> 
+        </div>
+    )
+}
+
+export default AdminDashboardCourses
