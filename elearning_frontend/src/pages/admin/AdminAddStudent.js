b1097191865f31c15da18bc1f117fd9cc53e59a3
jessymatta <jessy.w.matta@gmail.com>
2022-10-10 21:51:27 +0300

    Frontend <> Add Students : added the add students component

---
+++ elearning_frontend/src/pages/admin/AdminAddStudent.js
@@ -0,0 +1,25 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Title from "../../components/header/Title";
+import './admindash.css';
+import Footer from "../../components/footer/Footer";
+import Register from "../../components/register-form/Register";
+
+const AdminAddStudent = () => {
+    return (
+        <div class="admin-dash">
+            <Sidebar/>
+            <div className="dash-container">
+                <Title title={"Add Instructor "}/>
+                
+                <div className="table-container">
+                    <div className="table-title">Basic information</div>
+                    {/* <Register /> */}
+                    HERE
+                </div>
+                <Footer/>
+            </div> 
+        </div>
+    )
+}
+
+export default AdminAddStudent
