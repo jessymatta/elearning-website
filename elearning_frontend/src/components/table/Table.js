9d924e6dd83a5cac5f690b558d491d22930ced32
jessymatta <jessy.w.matta@gmail.com>
2022-10-10 13:38:06 +0300

    Frontend <>  Admin Dash. : added and styled the elements of the table component

--- elearning_frontend/src/components/table/Table.js
+++ elearning_frontend/src/components/table/Table.js
@@ -2,7 +2,41 @@ import "./table.css";
 
 const Table = () => {
     return (
-        <div className='table'>Table</div>
+        <div className='table'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Joining Date</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>2022001</td>
+                        <td>Instr. Name</td>
+                        <td>Instr. Email</td>
+                        <td>2022-Oct-03</td>
+                    </tr>
+
+                    <tr>
+                        <td>2022001</td>
+                        <td>Instr. Name</td>
+                        <td>Instr. Email</td>
+                        <td>2022-Oct-03</td>
+                    </tr>
+
+                    <tr>
+                        <td>2022001</td>
+                        <td>Instr. Name</td>
+                        <td>Instr. Email</td>
+                        <td>2022-Oct-03</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
