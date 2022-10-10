f7fea0a5b095d67b4a9bd6731fa7a2a03ba22de9
jessymatta <jessy.w.matta@gmail.com>
2022-10-10 14:00:26 +0300

    Frontend <> fetched data from the get all students api

--- elearning_frontend/src/components/table/Table.js
+++ elearning_frontend/src/components/table/Table.js
@@ -1,6 +1,23 @@
 import "./table.css";
+import axios from "axios";
 
 const Table = () => {
+
+    const token=localStorage.getItem('token');
+    console.log("--------- "+token);
+    function getAllInstructors() {
+        axios({
+            method: "get",
+            url: "http://127.0.0.1:8000/api/v0.1/admin/all_students",
+            headers: {Authorization: `Bearer${token}`},
+        }).then((response)=> {
+            console.log(response);
+
+        }).catch((error) => {
+            console.log(error.message);
+        });
+    }
+    getAllInstructors();
     return (
         <div className='table'>
             <table>
