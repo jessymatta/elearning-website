8a14b81dacd7bb7e214061443aa91f31d5af9631
jessymatta <jessy.w.matta@gmail.com>
2022-10-11 21:07:22 +0300

    Frontend <> Instr Add Assign. : finalized the add assignment form

--- elearning_frontend/src/components/register-form/AddAssignment.js
+++ elearning_frontend/src/components/register-form/AddAssignment.js
@@ -1,24 +1,25 @@
 import "./register.css";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const AddAssignment = () => {
 
     const token = localStorage.getItem('token');
+    const user_id = localStorage.getItem('id');;
     const [crn, setCRN] = useState("");
     const [description, setDescription] = useState("");
     const [due_date, setDueDate] = useState("");
     const navigate = useNavigate();
-    const [instructor_courses, setInstructorCourses] = useState([]);
+    const [courses_crn, setCourseCRN] = useState([]);
 
-    // useEffect(() => {
-    //     const getAssignments = async () => {
-    //         const instructors_from_server = await getCoursesAPI();
-    //         setAssignments(instructors_from_server);
-    //     }
-    //     getAssignments();
-    // }, []);
+    useEffect(() => {
+        const getCourseCRN = async () => {
+            const crn_from_server = await getCoursesCRNAPI();
+            setCourseCRN(crn_from_server);
+        }
+        getCourseCRN();
+    }, []);
 
     //When the submit button is clicked
     const submitCourse = async (e) => {
@@ -45,39 +46,43 @@ const AddAssignment = () => {
                 console.log(error.message);
             });
     };
-// TODO get courses api according to the logged in user
-    // const getCoursesAPI = async () => {
-    //     try {
 
-    //         const results = await axios({
-    //             method: "get",
-    //             url: "http://127.0.0.1:8000/api/v0.1/instructor/all_instructor_courses",
-    //             headers: { Authorization: `Bearer${token}` },
-    //         });
-    //         const data = await results.data.instructors;
-    //         return data;
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    const getCoursesCRNAPI = async () => {
+        try {
 
-    // getCoursesAPI();
+            const results = await axios({
+                method: "get",
+                url: `http://127.0.0.1:8000/api/v0.1/instructor/instr_courses/${user_id}`,
+                headers: { Authorization: `Bearer${token}` },
+            });
+            const data = await results.data.instructor_courses;
+            return data;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    // //When the cancel button is clicked
-    // const cancelInputs = (e) => {
-    //     e.preventDefault();
-    //     setCRN('');
-    //     setCourseName('');
-    // };
+    //When the cancel button is clicked
+    const cancelInputs = (e) => {
+        e.preventDefault();
+        setDescription('');
+    };
 
     return (
         <form onSubmit={submitCourse} className="registration-form">
-            <label htmlFor="crn">Course CRN <span className="red">*</span></label>
-            <input type="text" name="crn" id="crn" required="required" placeholder="Enter course crn"
-                onChange={(e) => {
-                    setCRN(e.target.value);
-                }}
-                value={crn} />
+
+
+
+            <label for="crn">Course CRN</label>
+            <select name="crn" id="crn" onChange={(e) => {
+                setCRN(e.target.value);
+            }}
+                value={crn}>
+                <option selected="true" disabled="disabled">Choose a course</option>
+                {courses_crn.map((course_crn) => (
+                    <option>{course_crn.crn}</option>
+                ))}
+            </select>
 
             <label htmlFor="description">Description <span className="red">*</span></label>
             <input type="text" name="description" id="description" required="required" placeholder="Enter description"
@@ -86,7 +91,7 @@ const AddAssignment = () => {
                 }}
                 value={description} />
 
-                
+
             <label htmlFor="due_date">Due Date <span className="red">*</span></label>
             <input type="date" name="due_date" id="due_date" required="required"
                 onChange={(e) => {
@@ -96,7 +101,7 @@ const AddAssignment = () => {
 
             <div className="action-btns">
                 <button type="submit" className="green-background">Submit</button>
-                <button  className="red-background margin-left">Cancel</button>
+                <button onClick={cancelInputs} className="red-background margin-left">Cancel</button>
             </div>
         </form>
     )
