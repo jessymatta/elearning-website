9f74ececd7a780516b848b6fcff7769e1cc45e88
jessymatta <jessy.w.matta@gmail.com>
2022-10-11 10:03:43 +0300

    Backend <> Assignments : added the instructor info to the response of the add assign. api

--- elearning_server/app/Http/Controllers/AssignmentController.php
+++ elearning_server/app/Http/Controllers/AssignmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Assignment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Course;
 
 
@@ -13,10 +14,12 @@ class AssignmentController extends Controller
     //A method that will add a new assignment
     public function addAssignment(Request $request)
     {
+        $instructor_name = Auth::user();
+
         $validator = Validator::make($request->all(), [
             'course_name' => 'required|string',
             'description' => 'required|string',
-            'due_date' => 'required|string'
+            'due_date' => 'required|string',
         ]);
 
         if ($validator->fails()) {
@@ -27,12 +30,13 @@ class AssignmentController extends Controller
             "course_name" => $request->course_name,
             "description" => $request->description,
             "due_date" => $request->due_date,
+            "course_instructor" => $instructor_name,
         ]);
 
         return response()->json([
-            'message'=>'Assignment successfully added',
-            'status'=>"success",
-            'assignment'=>$assignment
+            'message' => 'Assignment successfully added',
+            'status' => "success",
+            'assignment' => $assignment
         ], 201);
     }
 }
