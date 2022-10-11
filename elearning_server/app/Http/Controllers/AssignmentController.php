88a41151ee7334ca9018423067f0a9282d1e98ed
jessymatta <jessy.w.matta@gmail.com>
2022-10-11 09:32:37 +0300

    Backend <> Assignments : added a method that will add a new assignment

--- elearning_server/app/Http/Controllers/AssignmentController.php
+++ elearning_server/app/Http/Controllers/AssignmentController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assignment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Course;
+
 
 class AssignmentController extends Controller
 {
-    //
+    //A method that will add a new assignment
+    public function addAssignment(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'course_name' => 'required|string',
+            'description' => 'required|string',
+            'due_date' => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $assignment = Assignment::create([
+            "course_name" => $request->course_name,
+            "description" => $request->description,
+            "due_date" => $request->due_date,
+        ]);
+
+        return response()->json([
+            'message'=>'Assignment successfully added',
+            'status'=>"success",
+            'assignment'=>$assignment
+        ], 201);
+    }
 }
