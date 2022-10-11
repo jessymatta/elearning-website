473c165fb2e599f20f604da26d8a1aca1b2d7b77
jessymatta <jessy.w.matta@gmail.com>
2022-10-11 08:34:13 +0300

    Frontend <> Instr. Dash : fixed sidebar styling and links

--- elearning_frontend/src/components/sidebar/SidebarInstr.js
+++ elearning_frontend/src/components/sidebar/SidebarInstr.js
@@ -20,7 +20,7 @@ const SidebarInstr = () => {
 
         <div className='admin-sidebar'>
             <div className='top'>
-                <span className='logo'>UNI L<span className=' logo green'>O</span>G<span className='logo green'>O</span></span>
+                <span className='logo margin-top'>UNI L<span className=' logo green'>O</span>G<span className='logo green'>O</span></span>
                 <span className='role'>Instructor</span>
                 <span className='role'>@{username}</span>
             </div>
@@ -28,23 +28,23 @@ const SidebarInstr = () => {
             <div className='center'>
                 <ul>
 
-                    <p class="nav-title"><FontAwesomeIcon className="sidebar-icon" icon={faUsers} />STUDENTS</p>
-                    <Link class="default-link" to="/admin/students">
+                    <p class="nav-title margin-top-nav"><FontAwesomeIcon className="sidebar-icon" icon={faUsers} />STUDENTS</p>
+                    <Link class="default-link" to="/instructor">
                         <li><span>All Students</span></li>
                     </Link>
-                    <Link class="default-link" to="/admin/add_student">
+                    <Link class="default-link" to="">
                         <li><span>Add Student</span></li>
                     </Link>
 
                     <p class="nav-title"><FontAwesomeIcon className="sidebar-icon" icon={faFileCircleCheck} />ASSIGNMENTS</p>
-                    <Link class="default-link" to="/admin"><li><span>All Assignments</span></li></Link>
-                    <Link class="default-link" to="/admin/add_instructor"><li><span>Add Assignment</span></li></Link>
+                    <Link class="default-link" to=""><li><span>All Assignments</span></li></Link>
+                    <Link class="default-link" to=""><li><span>Add Assignment</span></li></Link>
 
                     <p class="nav-title"><FontAwesomeIcon className="sidebar-icon" icon={faBullhorn} />ANNOUNCEMENTS</p>
-                    <Link class="default-link" to="/admin/courses">
+                    <Link class="default-link" to="">
                         <li><span>All Announcements</span></li>
                     </Link>
-                    <Link class="default-link" to="/admin/add_course">
+                    <Link class="default-link" to="">
                         <li><span>Add Announcement</span></li>
                     </Link>
                 </ul>
