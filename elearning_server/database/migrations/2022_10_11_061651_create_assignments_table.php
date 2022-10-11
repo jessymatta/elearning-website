cdd4faf7ef6c5a1e2b274a23ff3646b3f9f0e0b1
jessymatta <jessy.w.matta@gmail.com>
2022-10-11 09:20:26 +0300

    Backend <> Assignments : modified the assignments collection

--- elearning_server/database/migrations/2022_10_11_061651_create_assignments_table.php
+++ elearning_server/database/migrations/2022_10_11_061651_create_assignments_table.php
@@ -6,24 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
         Schema::create('assignments', function (Blueprint $table) {
             $table->id();
+            $table->string('course_name');
+            $table->string('description');
+            $table->string('due_date');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         Schema::dropIfExists('assignments');
