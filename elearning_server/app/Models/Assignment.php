d6ec7589ad7c3231ad6d4acd8f9df61203d9d699
jessymatta <jessy.w.matta@gmail.com>
2022-10-11 09:25:14 +0300

    Backend <> Assignments : added the fillable array to the Assignment model

--- elearning_server/app/Models/Assignment.php
+++ elearning_server/app/Models/Assignment.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Assignment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'course_name',
+        'description',
+        'due_date',
+    ];
 }
